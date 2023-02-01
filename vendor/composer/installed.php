4830967f530816f7c06e0c743ce2584292454f88
Andi <evoszekt2437@gmail.com>
2023-02-01 14:29:54 +0700

    add:[AS] add profile page

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'laravel/laravel',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '578e81ec6e5143a93e4b236781aa90556fc4e00d',
+        'reference' => '74345c54d7d61f582f4805f63253f0d21a68ef75',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -367,7 +367,7 @@
         'laravel/laravel' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '578e81ec6e5143a93e4b236781aa90556fc4e00d',
+            'reference' => '74345c54d7d61f582f4805f63253f0d21a68ef75',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
@@ -709,8 +709,8 @@
         'psr/log-implementation' => array(
             'dev_requirement' => false,
             'provided' => array(
-                0 => '1.0.0 || 2.0.0 || 3.0.0',
-                1 => '1.0|2.0|3.0',
+                0 => '1.0|2.0|3.0',
+                1 => '1.0.0 || 2.0.0 || 3.0.0',
             ),
         ),
         'psr/simple-cache' => array(
