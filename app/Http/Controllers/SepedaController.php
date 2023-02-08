9129fe67823ec1cb019daab6323294d2f554682f
arilchasan <arilchasan@gmail.com>
2023-02-08 21:03:54 +0700

    add: [AC] add: sepeda models and controller,change routes sepeda

--- app/Http/Controllers/SepedaController.php
+++ app/Http/Controllers/SepedaController.php
@@ -2,17 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+use DB;
 use App\Models\Sepeda;
 use Illuminate\Http\Request;
 
 class SepedaController extends Controller
 {
-    public function index()
-    {
+    // public function index()
+    // {
   
-        return view('sepeda',[
-            "data" => Sepeda::all()
-        ]); 
-    }
-    
+    //     return view('sepeda',[
+    //         "data" => Sepeda::all()
+    //     ]); 
+    // }
+    // public function index(){
+    //     $sepeda = DB::select("select * from sepedas");
+    //     return view('sepeda',['data'=>$sepeda]);
+    //     }
+
+    public function index(){
+        $sepeda = Sepeda::all();
+        return view('sepeda',['data'=>$sepeda]);
+        }
+
 }
