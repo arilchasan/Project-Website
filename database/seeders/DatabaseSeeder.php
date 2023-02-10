d39e59f98a6df6d91bd9ffd32bf9c6f4ec6be102
arilchasan <arilchasan@gmail.com>
2023-02-10 20:02:20 +0700

    add: [AC] add seeder inserto table sepeda

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -41,6 +41,60 @@ class DatabaseSeeder extends Seeder
                     'harga' => 'Rp 2.999.000',
                     'desc' => 'Sepeda Dahon Ion Madison dibekali dengan frame berbahan alloy, yang ringan dan tahan karat. menggunakan ukuran ban 20 inci, sepeda ini cocok untuk kalangan remaja dan dewasa. Untuk menunjang kemudahan pengendara, Dahon Ion Madison ini difasilitasi 7 pilihan kecepatan dari Shimano untuk menghadapi berbagai kondisi jalan dan memberi fleksibilitas dalam berkendara. Menggunakan rem cakram mekanik yang pakem. Pegangan sepeda yang berbentuk ergonomis menyesuaikan kontur genggaman telapak tangan untuk meningkatkan kenyamanan, terutama saat berkendara lama. Terdapat mounting pada bagian headtube yang bisa digunakan untuk front block. Dilengkapi dengan magnet yang kuat di bagian roda depan dan belakang, untuk menjaga sepeda terlipat dengan aman. Sepeda lipat 20 inci ini cukup ringkas dan praktis disimpan.',
                     ),
+            array(
+                    'image' => 'image/curved.jpg',
+                    'merk' => 'Element Curved 700C"',
+                    'harga' => 'Rp 6.525.000',
+                    'desc' => '',
+                    ),
+            array(
+                    'image' => 'image/thunder.jpg',
+                    'merk' => 'Twitter Thunder Brake 700C"	',
+                    'harga' => 'Rp 21.500.000',
+                    'desc' => '',
+                    ),
+            array(
+                    'image' => 'image/london.jpg	',
+                    'merk' => 'London Taxi CRB M 700C"	',
+                    'harga' => 'Rp 5.024.000',
+                    'desc' => '',
+                    ),
+            array(
+                    'image' => 'image/espana.jpg',
+                    'merk' => 'United Espana 2 14"',
+                    'harga' => 'Rp 6.000.000',
+                    'desc' => '',
+                    ),
+            array(
+                    'image' => 'image/gravel.jpg',
+                    'merk' => 'Element FRC 38 Gravel 700C	',
+                    'harga' => 'Rp 4.050.000',
+                    'desc' => '',
+                    ),
+            array(
+                    'image' => 'image/spdpasific.jpeg',
+                    'merk' => 'Pacific Nimbuzz 14"',
+                    'harga' => 'Rp 6.200.000',
+                    'desc' => '',
+                    ),
+            array(
+                    'image' => 'image/unBiru.jpg',
+                    'merk' => 'United Stavros 27.5"',
+                    'harga' => 'Rp 2.850.000',
+                    'desc' => '',
+                    ),
+            array(
+                    'image' => 'image/unHanzo.jpg ',
+                    'merk' => 'United Hanzo',
+                    'harga' => 'Rp 1.700.000',
+                    'desc' => '',
+                    ),
+            array(
+                    'image' => 'image/unTank.jpg',
+                    'merk' => 'United Tank Discbrake',
+                    'harga' => 'Rp 1.750.000',
+                    'desc' => '',
+                    ),
             
             ),
             
